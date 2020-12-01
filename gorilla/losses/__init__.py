93935cd887bd65bb70101f9b540f16feab0ce295
lzhnb <liang13268932888@outlook.com>
2020-12-01 14:18:58 +0800

    move some loss func from fvcore

--- gorilla/losses/__init__.py
+++ gorilla/losses/__init__.py
@@ -1,5 +1,12 @@
 # Copyright (c) Gorilla-Lab. All rights reserved.
-
+from .focal_loss import (
+    sigmoid_focal_loss,
+    sigmoid_focal_loss_jit,
+    sigmoid_focal_loss_star,
+    sigmoid_focal_loss_star_jit,
+)
+from .giou_loss import giou_loss
+from .smooth_l1_loss import smooth_l1_loss
 
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
