513e542d597d5b13e7ce8df800e94f129f4549c6
lzhnb <liang13268932888@outlook.com>
2020-12-02 10:29:07 +0800

    remove the DA models and add resnet

--- gorilla/nn/__init__.py
+++ gorilla/nn/__init__.py
@@ -8,6 +8,11 @@ from .conv import GorillaConv
 from .FC import GorillaFC, MultiFC
 from .vgg import VGG
 from .alexnet import AlexNet
+from .resnet import (BasicBlock, Bottleneck, ResNet, conv3x3,
+                     resnet18, resnet34, resnet50, resnet101,
+                     resnet152, resnet)
+
+from .layer_builder import build_from_package, get_torch_layer_caller
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
 
--- gorilla/nn/models/build_model.py
+++
@@ -1,7 +0,0 @@
-from gorilla.nn.models.domain_adaptation import DANN
-
-def build_model(cfg):
-    if cfg.method == "DANN":
-        model = DANN(cfg)
-
-        return model
