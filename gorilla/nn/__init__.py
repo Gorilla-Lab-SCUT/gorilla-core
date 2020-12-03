3865b5dd7b0c081a079cd220d51fee6b5f52da6b
lzhnb <liang13268932888@outlook.com>
2020-12-03 12:06:24 +0800

    add the gcn(Graph Convolution Network)

--- gorilla/nn/__init__.py
+++ gorilla/nn/__init__.py
@@ -11,6 +11,7 @@ from .alexnet import AlexNet
 from .resnet import (BasicBlock, Bottleneck, ResNet, conv3x3,
                      resnet18, resnet34, resnet50, resnet101,
                      resnet152, resnet)
+from .gcn import GraphConvolution, GCN
 
 from .layer_builder import build_from_package, get_torch_layer_caller
 
