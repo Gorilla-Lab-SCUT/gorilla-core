3048ea4e2329036ee250d599286b200dbb81082a
zhjscut <806205254@qq.com>
2020-12-04 18:04:19 +0800

    add build_single_optimizer() and build_optimizer_v2() into build.py

--- gorilla/solver/build.py
+++ gorilla/solver/build.py
@@ -32,6 +32,50 @@ def build_optimizer(model: torch.nn.Module,
     return optimizer_caller(**cfg)
 
 
+def build_single_optimizer(
+        model: torch.nn.Module,
+        optimizer_cfg: [Config, Dict]) -> torch.optim.Optimizer:
+    r"""
+    Build a single optimizer from optimizer config, supporting multi parameter
+    groups with different setting in an optimizer
+    """
+    optimizer_type = optimizer_cfg.pop("type")
+    optimizer_cfg["params"] = []
+    if not hasattr(optimizer_cfg, "paramwise_cfg"):
+        # take the whole model parameters
+        optimizer_cfg["params"].append(
+            {"params": filter(lambda p: p.requires_grad, model.parameters())})
+    else:
+        for key, value in optimizer_cfg.paramwise_cfg.items():
+            optimizer_cfg["params"].append({
+                "params":
+                filter(lambda p: p.requires_grad,
+                       getattr(model, key).parameters()),
+                **value
+            })
+    optimizer_caller = getattr(torch.optim, optimizer_type)
+
+    return optimizer_caller(**optimizer_cfg)
+
+
+def build_optimizer_v2(model: torch.nn.Module,
+                       cfg: [Config, Dict]) -> torch.optim.Optimizer:
+    r"""
+    Build an optimizer from config, supporting multi optimizers.
+    If there is no omission, build_optimizer_v2 can take the place of
+    build_optimizer without changing the API
+    """
+    multi_optimizer = cfg.optimizer.pop("multi_optimizer", False)
+    if multi_optimizer:
+        optimizer_dict = {}
+        for key, optimizer_cfg in cfg.optimizer.items():
+            optimizer_dict[key] = build_single_optimizer(model, optimizer_cfg)
+        return optimizer_dict
+    else:
+        optimizer_cfg = cfg.optimizer
+        return build_single_optimizer(model, optimizer_cfg)
+
+
 def build_lr_scheduler(
         optimizer: torch.optim.Optimizer,
         cfg: [Config, Dict],
